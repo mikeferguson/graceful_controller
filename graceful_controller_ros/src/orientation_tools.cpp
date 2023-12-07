f08b884d5598df9207ca26716f2167e87a5243d4
Michael Ferguson <mfergs7@gmail.com>
2023-12-07 17:08:53 -0500

    fix build when RelWithDebInfo (#71)
    Not exactly sure what is going on here, but this closes #52
    
    Detailed discussion in https://github.com/ros2/geometry2/issues/639

--- graceful_controller_ros/src/orientation_tools.cpp
+++ graceful_controller_ros/src/orientation_tools.cpp
@@ -38,6 +38,7 @@
 
 #include <angles/angles.h>
 #include <rclcpp/logging.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2/utils.h>  // getYaw
 #include "graceful_controller_ros/orientation_tools.hpp"
 
